244d86aacf0cc391cb7b6b10b8f80fe22a917e69
B1naryB0t <47212121+MahdiTheGreat@users.noreply.github.com>
2021-11-16 13:08:25 +0330

    closer to the final version but there is a problem with stemming as it has trouble stemming when the texts are long

--- searchEngine.py
+++ searchEngine.py
@@ -1,22 +1,25 @@
 from __future__ import unicode_literals
 from hazm import *
+import pandas as pd
+
 
 
 
 class DocTerm:
 # DocTerm = a list of positions def __init__(self, docId, termId):
- def _init_(self,docId,termId):
+ def __init__(self,docId,termId):
   self.docId = docId
   self.termId = termId
   self.positions = []
 
  def insert(self, position):
   self.positions.append(position)
-  self.positions = sorted(self.positions),
+  self.positions = sorted(self.positions)
+  print()
 
 class Term:
 
- def __init_(self, id):
+ def __init__(self, id):
   # Term = a list of DocTerms def __init__(self, id):
   self.id = id
   self.docTerms = dict()
@@ -27,81 +30,112 @@ class Term:
    self.docTerms[docId].insert(position=position),
 
 class Index:
- def __init_(self, docs):
-  self.index = dict()
-  for i in range(len(docs)):
-   doc = docs[i]
-   for j in range(len (doc)):
-    word = doc[j]
-    if word not in self.index.keys():
-     self.index[word] = Term(id=word)
-    self.index[word].insert(docId=i, position=j),
 
-def distanseCaluclator(positions1,positions2,distance):
- freq=0
+ def __init__(self, docs):
+
+  self.index = dict()
+  stopWords=stopwords_list()
+
+  for i in range(len(docs)):
+   print()
+   doc = preProcessor(docs[i])
+   print()
+
+   for j in range(len(doc)):
+    word = doc[j]
+
+    if word not in stopWords:
+     if word not in self.index.keys():self.index[word] = Term(id=word)
+     self.index[word].insert(docId=i, position=j)
+
+    print()
+
+
+def correctWordDistanceChecker(positions1,positions2,distance):
+ print()
  for i in range(len(positions1)):
   for j in range(len(positions2)):
-   if positions1[i]-positions2[j]==distance:freq=next(freq)
- return freq
+   print()
+   if positions2[j]-positions1[i]==distance:
+       print()
+       return True
+ print()
+ return False
 
 def merge(index,t1,t2,d):
  commonDocs = []
  w1DocIds = sorted(index[t1].docTerms.keys())
  w1Docs=index[t1].docTerms
- w2DocIds = sorted(index[t2].docTerms.leys())
+ w2DocIds = sorted(index[t2].docTerms.keys())
  w2Docs = index[t2].docTerms
- freq=0
  i=0
  j=0
- while i<len(w1DocIds) and j<len(w1DocIds):
-     if i!=j:
-         if i<j:next(i)
-         else:next((j))
+ print()
+ while i<len(w1DocIds) and j<len(w2DocIds):
+     print()
+     if w1DocIds[i]!=w2DocIds[j]:
+         if w1DocIds[i]<w2DocIds[j]:
+             i+=1
+             print()
+         else:
+             j+=1
+             print()
      else:
-         if w1DocIds[i]==w2DocIds:
-          tempFreq=distanseCaluclator(w1Docs[i].positions,w2Docs[i].positions,d)
-          freq=freq+tempFreq
-          if tempFreq:commonDocs.append(w1DocIds[i])
+          checkResult=correctWordDistanceChecker(w1Docs[w1DocIds[i]].positions, w2Docs[w2DocIds[j]].positions, d)
+          print()
+          if checkResult:
+           commonDocs.append(w1DocIds[i])
+          i += 1
+          j += 1
+
+ print()
  return commonDocs
 
 
-def query(index,terms):
+def textQuery(index,terms,mergeCache):
  commonDocs=set()
- for i in range(len(terms)):
-  commonDocs=commonDocs+set(merge(index,terms[0],terms[i],i))
+
+ for i in range(1,len(terms),1):
+
+  if (terms[0],terms[i]) not in mergeCache.keys():
+   mergeResult=set(merge(index,terms[0],terms[i],i))
+   mergeCache[(terms[0],terms[i])]=mergeResult
+   print()
+
+  commonDocs=set.union(commonDocs,mergeCache[(terms[0],terms[i])])
+  print()
+
  return commonDocs
 
+def queryResults(index,query):
+ queries=[]
+ mergeCache=dict()
+ terms=preProcessor(query)
+ print()
+ for i in range(len(terms),0,-1):
+  j=0
+  while j+i<=len(terms):
+   termsTemp=terms[j:j+i]
+   j+=1
+   print()
+   queries.append(textQuery(index,termsTemp,mergeCache))
+  queries.append('*')
 
-normalizer = Normalizer()
-normalizer.normalize('اصلاح نويسه ها و استفاده از نیم‌فاصله پردازش را آسان مي كند')
-
-sent_tokenize('ما هم برای وصل کردن آمدیم! ولی برای پردازش، جدا بهتر نیست؟')
-
-word_tokenize('ولی برای پردازش، جدا بهتر نیست؟')
-
-stemmer = Stemmer()
-stemmer.stem('کتاب‌ها')
+def preProcessor(text):
+ stemmer = Stemmer()
+ stemResult = stemmer.stem(text)
+ normalizer = Normalizer()
+ normalResult = normalizer.normalize(stemResult)
+ tokens = word_tokenize(normalResult)
+ return tokens
 
 
-#def query(index,word1,word2,k):
-# ans = []
-# p1 = index[word1].docTerms
-# p2 = index[word2].docTerms
-# while p1 is not None and p2 is not None:
-#  if p1.docId == p2.docId:
-#   l = []
-#   pp1 = p1.positions
-#   pp2 = p2.positions
-#   while pp1 is not None:
-#       while pp2 is not None:
-#           if abs(pp1.curpos - pp2.curPos) <= k:l.append(pp2.curPos)
-#           elif pp2.curpos > pp1.curPos:break
-#           pp2 = next(pp2)
-#       while l is not [] and abs(l[0] - pp1.curPos) > k:
-#           delete(l[0])
-#       for ps in l:
-#           ans.append([p1.docid, pp1.curPos, ps]) pp1 = next(pp1)
-#   p1 = next(p1)
-#   p2 = next(p2)
-#   elif p1.docId < p2.docId:p1 = next(p1)
-#   else:p2 = next(p2)
+docsDatabaseFileName="IR1_7k_news.xlsx"
+test='قرآن و اصلاح نويسه ها و استفاده از نیم‌فاصله پردازش را آسان مي كند'
+queryTest="مرزبان مربی سپاهان"
+#test=preProcessor(test)
+docs = pd.read_excel(docsDatabaseFileName)
+docs=list(docs['content'])
+index=Index(docs).index
+results=queryResults(index,queryTest)
+print()
